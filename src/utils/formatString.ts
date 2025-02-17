2cc868b46aeaa6acd5c735f45b23e83009814d84
Aerizel <jobrunbusiness@gmail.com>
2025-02-17 15:36:41 +0700

    add more exception error in organize name.

--- src/utils/formatString.ts
+++ src/utils/formatString.ts
@@ -9,11 +9,13 @@ export const cutStringWithDash = (text: string): string | null => {
     // if (parts.length > 2) {
     //     const word = parts[2].trim();
     //     return result;
-    
-    if(org_name) {
-        return org_name
+
+    if (org_name) {
+        //REMOVE ANY BRACKET AND VALUE INSIDE IT 
+        const result = org_name.replace(/\s*\([^)]*\)/g, ''); // Removes ' (anything inside)'
+        return result;
     } else {
         return null
     }
-    
+
 }
