b01a9fffe0cf6e2c2a691b0bd99bb42f89a3f8af
Aerizel <jobrunbusiness@gmail.com>
2025-02-27 16:25:10 +0700

    Fix user info sheet function.

--- src/utils/formatString.ts
+++ src/utils/formatString.ts
@@ -3,7 +3,7 @@ export const cutStringWithDash = (text: string): string | null => {
     //REMOVE FILE TYPE
     const rawName = text.replace(/\.xlsx$/, "");
     // SECONDS METHOD SPLIT STRING BY DASH INTO ARRAY THEN PICK VALUE OF LAST OF INDEX 
-    const org_name = rawName.split(/[-= _]+/).pop();
+    const org_name = rawName.split(/[-=_]+/).pop();
 
     // FIRST METHOD SPLIT STRING BY DASH INTO ARRAY THEN PICK VALUE OF THIRD OF INDEX 
     // if (parts.length > 2) {
